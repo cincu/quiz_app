c311ba75b39569751d36be216892d163bb7df62e
CANSU CANER <CANANCANSUCANER@gmail.com>
2023-08-26 18:04:53 +0200

    quiz-app 1 commit before complete

--- js/indexform.js
+++ js/indexform.js
@@ -8,10 +8,10 @@
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
     console.log('data', data);
-
     create();
     form.reset();
     event.target.elements.question.focus();
+    alert('thanks for the contribution!');
   });
 
   
@@ -22,14 +22,14 @@
     sectionToAppend.className = "section-card"
     
     const newTitle = document.createElement('h3')
-    newTitle.textContent = ("question")
+    newTitle.textContent = `new question: ${form.question.value}`
 
     const newAnswer = document.createElement('p')
-    newAnswer.textContent = ("answer")
+    newAnswer.textContent = `new answer: ${form.answer.value}`
 
     const newTags = document.createElement('li')
-    newTags.textContent = ("tags")
-
+    newTags.textContent = `new tags: ${form.tags.value}`
+    
     newTitle.className = "section-card__title"
     newAnswer.className = "section-card__content"
     newTags.className = "section-card__hint"
@@ -37,6 +37,20 @@
     
     sectionToAppend.append(newTitle, newAnswer, newTags)
     bodyMain.append(sectionToAppend)
-    
   };
+
+  //challenge4:
+const questionCharCount = document.querySelector('[data-js="text-input"]');
+const charCount = document.querySelector('[data-js="charCount"]');
+const maxLength = questionCharCount.getAttribute('maxlength');
+
+questionCharCount.addEventListener('input', updateCharCount);
+
+function updateCharCount () {
+  const currentLength = questionCharCount.value.length
+  const charLeft = maxLength - currentLength
+  charCount.textContent = `Characters left: ${charLeft}`
+}
+
+questionCharCount.addEventListener('input', updateCharCount);
 
